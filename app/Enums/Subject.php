95c36f36ee8b7861e3df65665d35974781f6bfc7
Viktor Demčák <viktorik170@gmail.com>
2024-01-07 13:21:28 +0100

    Added teacher, year and subject enums

---
+++ app/Enums/Subject.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Enums;
+
+enum Subject: string
+{
+    case ANJ = "ANJ";
+    case ANL = "ANL";
+    case BIO = "BIO";
+    case CHE = "CHE";
+    case cBIO = "cBIO";
+    case cCHE = "cCHE";
+    case cFYZ = "cFYZ";
+    case cGEG = "cGEG";
+    case cMAT = "cMAT";
+    case DES = "DES";
+    case DEJ = "DEJ";
+    case DEU = "DEU";
+    case EFI = "EFI";
+    case ETV = "ETV";
+    case EVN = "EVN";
+    case FIG = "FIG";
+    case FYZ = "FYZ";
+    case FyV = "FyV";
+    case GEG = "GEG";
+    case GZC = "GZC";
+    case HUV = "HUV";
+    case INF = "INF";
+    case KAJ = "KAJ";
+    case KNJ = "KNJ";
+    case MAT = "MAT";
+    case NAB = "NAB";
+    case NEJ = "NEJ";
+    case OBN = "OBN";
+    case PMO = "PMO";
+    case PRO = "PRO";
+    case PSE = "PSE";
+    case PSY = "PSY";
+    case RET = "RET";
+    case RUS = "RUS";
+    case sBIO = "sBIO";
+    case sCHE = "sCHE";
+    case sDEJ = "sDEJ";
+    case sEKO = "sEKO";
+    case sFYZ = "sFYZ";
+    case sGEG = "sGEG";
+    case sINF = "sINF";
+    case sMAT = "sMAT";
+    case sNEJ = "sNEJ";
+    case sSJL = "sSJL";
+    case sSJA = "sSJA";
+    case SVS = "SVS";
+    case SJA = "SJA";
+    case SJL = "SJL";
+    case TECH = "TECH";
+    case TvP = "TvP";
+    case UaK = "UaK";
+    case VYV = "VYV";
+    case OTHER = "other";
+}
---
+++ app/Enums/Year.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+enum Year: string
+{
+    case ROCNIK_5 = "5";
+    case PRIMA = "prima";
+    case SEKUNDA = "sekunda";
+    case TERCIA = "tercia";
+    case KVARTA = "kvarta";
+    case KVINTA = "kvinta";
+    case SEXTA = "sexta";
+    case SEPTIMA = "septima";
+    case OKTAVA = "oktava";
+    case OTHER = "other";
+}
