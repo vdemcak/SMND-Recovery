548887c0753ca2d42cebbeef24fa053da72f9119
Viktor Demčák <viktorik170@gmail.com>
2024-01-07 21:03:24 +0100

    Moved portal logic to portal controller

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\HomeController;
-use App\Models\Materials;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\PortalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,13 +21,7 @@ Route::get('/auth/redirect', [AuthController::class, 'redirect'])->name('login')
 Route::get('/auth/callback', [AuthController::class, 'callback']);
 Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/portal', function () {
-    return view('pages.portal', [
-        'material_count' => Materials::count(),
-        'file_count' => DB::table('materials')->selectRaw("SUM(cardinality(string_to_array(files, ','))) as total_sum")->value('total_sum')
-    ]);
-})->middleware('auth')->name('portal');
-
+Route::get('/portal', [PortalController::class, 'index'])->middleware('auth')->name('portal');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/', [HomeController::class, 'submit'])->name('home.submit');
