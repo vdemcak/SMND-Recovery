b016cfd32d3420c285065130bfec74c4feec2c74
Viktor Demčák <viktorik170@gmail.com>
2024-01-07 21:07:15 +0100

    Fixed home title

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
     <div class="flex flex-col max-w-3xl mx-auto w-full items-center">
-        <h1 class="text-4xl font-bold">Odovzdané materiály</h1>
+        <h1 class="text-4xl font-bold">Obnova edukačných materiálov</h1>
         <p class="mt-2 text-center leading-6 text-gray-600 w-4/5">
             V dôsledku vyhorenia zborovne sa stratila vysoká kvantita edukačných materiálov. Ak by ste si našli chvíľku,
             aby ste nám pomohli s obnovou, boli by sme veľmi vďační.
