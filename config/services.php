d108b1220a08f9820819e8f6472e546909c71596
Viktor Demčák <viktorik170@gmail.com>
2024-01-11 16:36:18 +0100

    Moved teacher role to service config, renamed app, edited example env

--- config/services.php
+++ config/services.php
@@ -6,5 +6,6 @@ return [
         'client_secret' => env('AZURE_CLIENT_SECRET'),
         'redirect' => env('AZURE_REDIRECT_URI'),
         'tenant' => env('AZURE_TENANT_ID'),
+        'teacher_role' => env('AZURE_TEACHER_ROLE'),
     ],
 ];
